4a462c5f01a7f1d4f7e9fccc87ef7026551fc24c
sugardrunk <jannekahkonen@gmail.com>
2014-09-07 10:07:20 +0300

    clean up redirect a bit

--- lib/redirect.php
+++ lib/redirect.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- * TODO.
+ * A simple redirect function.
  *
  */
-function my_page_template_redirect()
+function event_worker_frontpage_redirect()
 {
-    if(is_front_page() && is_page('events') )
+    if(is_front_page() && is_page('events'))
     {
-        wp_redirect( home_url( '/events/' ) );
+        wp_redirect(home_url('/events/'));
         exit();
     }
 }
-add_action( 'template_redirect', 'my_page_template_redirect' );
+add_action('template_redirect', 'event_worker_frontpage_redirect');
 
 ?>
